18c638e822185519cc220ac5d09bfedbda3cfbec
sachin.kaushik1 <sachin.kaushik@prismberry.com>
2024-02-02 11:06:00 +0530

    project start

---
+++ my-ts-react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Practicing React with Typescript.</h1>
+    </div>
+  );
+}
+
+export default App;
